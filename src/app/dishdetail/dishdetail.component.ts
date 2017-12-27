a8cd4afa9a064898d4d2450c6ea805131349ff2f
anusha <akuna001@odu.edu>
2017-12-26 22:41:21 -0500

    created angular reactive form to take comments on the dish

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -5,6 +5,11 @@ import { DishService } from '../services/dish.service';
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 
+import { Comment } from '../shared/comment';
+
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+
 import 'rxjs/add/operator/switchMap';
 
 @Component({
@@ -18,10 +23,76 @@ export class DishdetailComponent implements OnInit {
   dishIds: number[];
   prev: number;
   next: number;
+  
+  commentForm: FormGroup;
+  comm: Comment;
+  
+  formErrors = {
+    'comment': '',
+    'author': ''
+  };
 
+
+validationMessages = {
+    'comment': {
+      'required':      'Comment is required.',
+      'maxlength':     'FirstName cannot be more than 125 characters long.'
+    },
+    'author': {
+      'required':      'Your name is required.',
+      'minlength':     'Your name must be at least 2 characters long.',
+      'maxlength':     'Your Name cannot be more than 25 characters long.'
+    },
+  };
+  
+  
   constructor(private dishservice: DishService,
     private route: ActivatedRoute,
-    private location: Location) { }
+    private location: Location, private fb: FormBuilder) {
+	
+	this.createForm();
+	}
+	
+	
+	createForm() {
+   this.commentForm = this.fb.group({
+      comment: ['', [Validators.required, Validators.maxLength(125)] ],
+      author: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)] ]
+    });
+	
+	 this.commentForm.valueChanges
+      .subscribe(data => this.onValueChanged(data));
+	  
+	  this.onValueChanged();
+
+   
+  }
+  
+  onValueChanged(data?: any) {
+    if (!this.commentForm) { return; }
+    const form = this.commentForm;
+    for (const field in this.formErrors) {
+      // clear previous error message (if any)
+      this.formErrors[field] = '';
+      const control = form.get(field);
+      if (control && control.dirty && !control.valid) {
+        const messages = this.validationMessages[field];
+        for (const key in control.errors) {
+          this.formErrors[field] += messages[key] + ' ';
+        }
+      }
+    }
+  }
+  
+  onSubmit() {
+    this.comm = this.commentForm.value;
+    console.log(this.comm);
+    this.commentForm.reset({
+      author: '',
+      rating: '',
+      comment: ''
+    });
+  }
 
   ngOnInit() {
     let id = +this.route.snapshot.params['id'];
@@ -33,6 +104,8 @@ export class DishdetailComponent implements OnInit {
       .subscribe(dish => { this.dish = dish; this.setPrevNext(dish.id); });
 	  
   }
+  
+  
   
   setPrevNext(dishId: number) {
     let index = this.dishIds.indexOf(dishId);
